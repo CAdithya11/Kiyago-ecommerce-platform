f26ba22b6004a994c5b5406198e19143b6fe867f
CAdithya11 <cadithya13@gmail.com>
2025-07-30 11:25:32 +0530

    issue fixed with navbar location cna't identify

--- src/component/layout/navbar/NavBar.jsx
+++ src/component/layout/navbar/NavBar.jsx
@@ -1,6 +1,6 @@
 import { Menu, Search, ShoppingBag, User } from 'lucide-react';
 import logo from '../../../assets/logo/logo.png';
-import '../../../css/home/NavBar.css';
+import './NavBar.css';
 import { Link } from 'react-router-dom';
 
 export default function NavBar({ scrolled }) {
